51698d53973db511f729562c9511d7603d14f236
Naufal Fachrian <naufal.fachrian@gmail.com>
2021-08-22 13:34:14 +0700

    Add paging data source

---
+++ app/src/main/java/com/naufalfachrian/awesomeapp/PexelsCuratedPagingDataSource.kt
@@ -0,0 +1,27 @@
+package com.naufalfachrian.awesomeapp
+
+import androidx.paging.PagingState
+import androidx.paging.rxjava2.RxPagingSource
+import com.naufalfachrian.pexels_api.PexelsApiService
+import com.naufalfachrian.pexels_api.entities.PexelsPhoto
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+
+class PexelsCuratedPagingDataSource : RxPagingSource<Int, PexelsPhoto>() {
+
+    override fun getRefreshKey(state: PagingState<Int, PexelsPhoto>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+    override fun loadSingle(params: LoadParams<Int>): Single<LoadResult<Int, PexelsPhoto>> {
+        val pageNumberToLoad = params.key ?: 1
+        return PexelsApiService.instance.fetchCuratedPhotos(pageNumberToLoad, params.loadSize)
+            .subscribeOn(Schedulers.io())
+            .map<LoadResult<Int, PexelsPhoto>> { response -> LoadResult.Page(response.photos, null, response.page + 1) }
+            .singleOrError()
+    }
+
+}
